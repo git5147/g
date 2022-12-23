818845fb2c5200623b50f3d674a32cfc5f3bcdba
user5148 <121293465+user5148@users.noreply.github.com>
2022-12-23 22:20:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,63 @@
 // Set the access token and API endpoint for the GitHub API
-var accessToken = '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';
+//var accessToken = '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';
 //var apiEndpoint = 'https://api.github.com/repos/user5148/ul/index.txt';
-var apiEndpoint = 'https://api.github.com/repos/user5148/ul/contents/';
+//var apiEndpoint = 'https://api.github.com/repos/user5148/ul/contents/';
 
-// Get the user's location when the button is clicked
-document.getElementById('button').addEventListener('click', function () {
-  // Get the current time
-  var time = new Date().toString();
+const accessToken = "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";
+const username = "user5148";
+const repository = "ul";
 
-  // Get the user's location
+const button = document.getElementById("getLocationButton");
+button.addEventListener("click", getLocation);
+
+function getLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      var latitude = position.coords.latitude;
-      var longitude = position.coords.longitude;
-      var location = 'Latitude: ' + latitude + '\nLongitude: ' + longitude;
-      createFile(location, time);
-    });
+    navigator.geolocation.getCurrentPosition(showPosition);
   } else {
-    console.log('Geolocation is not supported by this browser.');
+    console.log("Geolocation is not supported by this browser.");
   }
-});
+}
 
-// Create a new file with the location and time
-function createFile(location, time) {
-  // Get the current date and time
-  var currentDate = new Date();
-  // Format the date and time as a string in the format "YYYY-MM-DD_HH-mm-ss"
-  var fileName = currentDate.getFullYear() + '-' + (currentDate.getMonth() + 1).toString().padStart(2, '0') + '-' + currentDate.getDate().toString().padStart(2, '0') + '_' + currentDate.getHours().toString().padStart(2, '0') + '-' + currentDate.getMinutes().toString().padStart(2, '0') + '-' + currentDate.getSeconds().toString().padStart(2, '0');
+function showPosition(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const location = `Latitude: ${latitude}  Longitude: ${longitude}`;
+  document.getElementById("location").innerHTML = location;
 
-  // Send a request to the GitHub API to create a new file with the location and time
-  var request = new XMLHttpRequest();
-  request.open('POST', apiEndpoint);
-  request.setRequestHeader('Authorization', 'Token ' + accessToken);
-  request.setRequestHeader('Content-Type', 'application/json');
-  request.onreadystatechange = function () {
-    if (this.readyState === 4) {
-      console.log(this.responseText);
-    }
+  createFile(location);
+}
+
+function createFile(location) {
+  const date = new Date();
+  const filename = `location-${date.toISOString()}.txt`;
+  const content = location;
+  const contentType = "text/plain";
+  const commitMessage = "Add location data";
+
+  const data = {
+    message: commitMessage,
+    content: btoa(content),
   };
-  var data = JSON.stringify({
-    'message': 'Add location data',
-    'content': btoa(location + '\n' + time)
-  });
-  request.send(data);
+
+  fetch(
+    `https://api.github.com/repos/${username}/${repository}/contents/${filename}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": contentType,
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify(data),
+    }
+  )
+    .then((response) => {
+      if (response.ok) {
+        console.log(`File "${filename}" created successfully.`);
+      } else {
+        console.error("Failed to create file.");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
